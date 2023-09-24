ac3113b52c79119c8f7464daa9ef22e80a47d4c7
einzero12 <145881044+einzero12@users.noreply.github.com>
2023-09-23 22:58:00 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,64 +1,3 @@
-const betaFish = document.getElementById('beta-fish');
-const fishFood = document.getElementById('fish-food');
-
-let speed = 1; // Adjust the speed as needed
-
-// Define the sequence of movements
-const movements = [
-    { angle: 0, distance: 500 }, // Move right
-    { angle: 110, distance: 300 }, // Move up at a 110-degree angle
-    { angle: 210, distance: 200 }, // Move down at a 210-degree angle
-    { angle: 270, distance: 250 } // Move down at a 270-degree angle
-];
-
-let currentMovement = 0;
-let distanceMoved = 0;
-
-function moveFish() {
-    const movement = movements[currentMovement];
-    const radianAngle = movement.angle * (Math.PI / 180);
-    
-    const deltaX = speed * Math.cos(radianAngle);
-    const deltaY = -speed * Math.sin(radianAngle);
-    
-    betaFish.style.left = (betaFish.offsetLeft + deltaX) + 'px';
-    betaFish.style.top = (betaFish.offsetTop + deltaY) + 'px';
-    
-    distanceMoved += speed;
-    if (distanceMoved >= movement.distance) {
-        distanceMoved = 0;
-        currentMovement = (currentMovement + 1) % movements.length;
-    }
-    
-    requestAnimationFrame(moveFish);
-}
-
-fishFood.addEventListener('click', function() {
-    const flake = document.createElement('img');
-    flake.src = 'https://github.com/einzero12/petfish/blob/main/flake.png?raw=true';
-    flake.className = 'flake';
-    
-    // Position the flake in the middle of the screen and 300px down
-    flake.style.left = (window.innerWidth / 2 - flake.width / 2) + 'px';
-    flake.style.top = '300px';
-    
-    document.body.appendChild(flake);
-    
-    function fall() {
-        flake.style.top = (flake.offsetTop + 1) + 'px';
-        requestAnimationFrame(fall);
-    }
-    
-    fall();
-}
-
-// Center the fish initially
-betaFish.style.left = (window.innerWidth / 2 - betaFish.width / 2) + 'px';
-betaFish.style.top = (window.innerHeight / 2 - betaFish.height / 2) + 'px';
-
-// Start the fish movement
-moveFish();
-
 // Check if Web3 is injected by MetaMask or another wallet
 if (window.ethereum) {
     window.web3 = new Web3(window.ethereum);
@@ -73,9 +12,9 @@ document.getElementById('connect-wallet').addEventListener('click', async () => 
             // Request account access
             await window.ethereum.enable();
 
-            const contractAddress = '0x1468fdac6c7a8ec9faffaf65c3bfb0b8e4a3f2fe.'; // Replace with your contract address
-            const tokenId = '1'; // The ID of the token you want to check for
-            const contractABI = [[{"inputs":[{"internalType":"address","name":"_logic","type":"address"},{"internalType":"address","name":"admin_","type":"address"},{"internalType":"bytes","name":"_data","type":"bytes"}],"stateMutability":"payable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"address","name":"previousAdmin","type":"address"},{"indexed":false,"internalType":"address","name":"newAdmin","type":"address"}],"name":"AdminChanged","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"beacon","type":"address"}],"name":"BeaconUpgraded","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"implementation","type":"address"}],"name":"Upgraded","type":"event"},{"stateMutability":"payable","type":"fallback"},{"inputs":[],"name":"admin","outputs":[{"internalType":"address","name":"admin_","type":"address"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"newAdmin","type":"address"}],"name":"changeAdmin","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"implementation","outputs":[{"internalType":"address","name":"implementation_","type":"address"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"newImplementation","type":"address"}],"name":"upgradeTo","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"newImplementation","type":"address"},{"internalType":"bytes","name":"data","type":"bytes"}],"name":"upgradeToAndCall","outputs":[],"stateMutability":"payable","type":"function"},{"stateMutability":"payable","type":"receive"}]]; // Replace with your contract ABI
+            const contractAddress = '0x1468fdac6c7a8ec9faffaf65c3bfb0b8e4a3f2fe'; 
+            const tokenId = '1'; 
+            const contractABI = [[{"inputs":[{"internalType":"address","name":"_logic","type":"address"},{"internalType":"address","name":"admin_","type":"address"},{"internalType":"bytes","name":"_data","type":"bytes"}],"stateMutability":"payable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"address","name":"previousAdmin","type":"address"},{"indexed":false,"internalType":"address","name":"newAdmin","type":"address"}],"name":"AdminChanged","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"beacon","type":"address"}],"name":"BeaconUpgraded","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"implementation","type":"address"}],"name":"Upgraded","type":"event"},{"stateMutability":"payable","type":"fallback"},{"inputs":[],"name":"admin","outputs":[{"internalType":"address","name":"admin_","type":"address"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"newAdmin","type":"address"}],"name":"changeAdmin","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"implementation","outputs":[{"internalType":"address","name":"implementation_","type":"address"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"newImplementation","type":"address"}],"name":"upgradeTo","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"newImplementation","type":"address"},{"internalType":"bytes","name":"data","type":"bytes"}],"name":"upgradeToAndCall","outputs":[],"stateMutability":"payable","type":"function"},{"stateMutability":"payable","type":"receive"}]]; // Replace with your actual contract ABI
 
             const contract = new web3.eth.Contract(contractABI, contractAddress);
             const userAddress = await web3.eth.getAccounts().then(accounts => accounts[0]);
@@ -97,5 +36,57 @@ document.getElementById('connect-wallet').addEventListener('click', async () => 
         console.log('Non-Ethereum browser detected. You should consider trying MetaMask!');
     }
 });
+
+// Fish Animation
+const fish = document.getElementById('beta-fish');
+let fishDirection = 110;
+let fishPositionX = window.innerWidth / 2;
+let fishPositionY = window.innerHeight / 2;
+
+function moveFish() {
+    fishPositionX += Math.cos(fishDirection * (Math.PI / 180)) * 3;
+    fishPositionY -= Math.sin(fishDirection * (Math.PI / 180)) * 3;
+    
+    // Check boundaries and change direction
+    if (fishPositionX < 0 || fishPositionX > window.innerWidth || fishPositionY < 0 || fishPositionY > window.innerHeight) {
+        fishDirection = (fishDirection + 135) % 360;
+    }
+
+    fish.style.left = fishPositionX + 'px';
+    fish.style.top = fishPositionY + 'px';
+}
+
+setInterval(moveFish, 1000 / 60);
+
+// Fish Food Rotation and Flake Falling
+const fishFood = document.getElementById('fish-food');
+
+fishFood.addEventListener('mouseover', () => {
+    fishFood.style.transform = 'rotate(120deg)';
+});
+
+fishFood.addEventListener('click', () => {
+    const flake = document.createElement('img');
+    flake.src = 'https://github.com/einzero12/petfish/blob/main/flake.png?raw=true';
+    flake.style.position = 'absolute';
+    flake.style.left = fishFood.getBoundingClientRect().left + 'px';
+    flake.style.top = fishFood.getBoundingClientRect().bottom + 10 + 'px';
+    flake.style.width = '50px';
+    document.body.appendChild(flake);
+
+    function fallFlake() {
+        const flakeRect = flake.getBoundingClientRect();
+        if (flakeRect.top < window.innerHeight) {
+            flake.style.top = flakeRect.top + 5 + 'px';
+        } else {
+            document.body.removeChild(flake);
+            clearInterval(falling);
+        }
+    }
+
+    const falling = setInterval(fallFlake, 1000 / 60);
+});
+
+
 
 
