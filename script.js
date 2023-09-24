7dc6412184ad1e6abcf13d9630fae2d5fff983be
einzero12 <145881044+einzero12@users.noreply.github.com>
2023-09-23 21:36:53 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,44 +1,53 @@
 const betaFish = document.getElementById('beta-fish');
 const fishFood = document.getElementById('fish-food');
-let direction = 'up';
-let distanceMoved = 0;
+const fishbowl = document.getElementById('fishbowl');
+
+let directionX = 'right';
+let directionY = 'down';
+let speed = 1; // Adjust the speed as needed
 
 function moveFish() {
-  const currentPosition = betaFish.offsetTop;
-  
-  if (direction === 'up') {
-    betaFish.style.top = (currentPosition - 1) + 'px';
-    distanceMoved += 1;
-    if (distanceMoved >= 100) {
-      distanceMoved = 0;
-      direction = 'down';
+    const fishPositionX = betaFish.offsetLeft;
+    const fishPositionY = betaFish.offsetTop;
+    const fishbowlPositionX = fishbowl.offsetLeft;
+    const fishbowlPositionY = fishbowl.offsetTop;
+    
+    if (directionX === 'right') {
+        betaFish.style.left = (fishPositionX + speed) + 'px';
+        if (fishPositionX >= fishbowlPositionX + 900) directionX = 'left';
+    } else {
+        betaFish.style.left = (fishPositionX - speed) + 'px';
+        if (fishPositionX <= fishbowlPositionX) directionX = 'right';
     }
-  } else if (direction === 'down') {
-    betaFish.style.top = (currentPosition + 1) + 'px';
-    distanceMoved += 1;
-    if (distanceMoved >= 100) {
-      distanceMoved = 0;
-      direction = 'up';
+    
+    if (directionY === 'down') {
+        betaFish.style.top = (fishPositionY + speed) + 'px';
+        if (fishPositionY >= fishbowlPositionY + 900) directionY = 'up';
+    } else {
+        betaFish.style.top = (fishPositionY - speed) + 'px';
+        if (fishPositionY <= fishbowlPositionY) directionY = 'down';
     }
-  }
-  
-  requestAnimationFrame(moveFish);
+    
+    requestAnimationFrame(moveFish);
 }
 
 fishFood.addEventListener('click', function() {
-  const flake = document.createElement('img');
-  flake.src = 'https://github.com/einzero12/petfish/blob/main/flake.png?raw=true';
-  flake.className = 'flake';
-  flake.style.top = (fishFood.offsetTop + 10) + 'px';
-  flake.style.left = fishFood.offsetLeft + 'px';
-  document.body.appendChild(flake);
-  
-  function fall() {
-    flake.style.top = (flake.offsetTop + 1) + 'px';
-    requestAnimationFrame(fall);
-  }
-  
-  fall();
+    const flake = document.createElement('img');
+    flake.src = 'https://github.com/einzero12/petfish/blob/main/flake.png?raw=true';
+    flake.className = 'flake';
+    
+    // Position the flake in the middle of the screen and 300px down
+    flake.style.left = (window.innerWidth / 2 - flake.width / 2) + 'px';
+    flake.style.top = '300px';
+    
+    document.body.appendChild(flake);
+    
+    function fall() {
+        flake.style.top = (flake.offsetTop + 1) + 'px';
+        requestAnimationFrame(fall);
+    }
+    
+    fall();
 });
 
 moveFish();
