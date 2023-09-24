acd593c0d232157fc3e9767e00930156221f8d23
einzero12 <145881044+einzero12@users.noreply.github.com>
2023-09-23 22:16:58 -0400

    Update script.js

--- script.js
+++ script.js
@@ -50,7 +50,7 @@ fishFood.addEventListener('click', function() {
     }
     
     fall();
-});
+}
 
 // Center the fish initially
 betaFish.style.left = (window.innerWidth / 2 - betaFish.width / 2) + 'px';
@@ -58,5 +58,37 @@ betaFish.style.top = (window.innerHeight / 2 - betaFish.height / 2) + 'px';
 
 // Start the fish movement
 moveFish();
+
+
+document.getElementById('connect-wallet').addEventListener('click', async () => {
+    if (window.ethereum) {
+        window.web3 = new Web3(window.ethereum);
+        try {
+            // Request account access
+            await window.ethereum.enable();
+
+            const contractAddress = '0x...'; // The address of your NFT contract
+            const tokenId = '1'; // The ID of the token you want to check for
+            const contractABI = [...] // The ABI of your NFT contract
+
+            const contract = new web3.eth.Contract(contractABI, contractAddress);
+            const userAddress = await web3.eth.getAccounts().then(accounts => accounts[0]);
+
+            contract.methods.ownerOf(tokenId).call().then(owner => {
+                if (owner === userAddress) {
+                    console.log('User owns the NFT!');
+                    // Unlock the content or navigate to the main page
+                } else {
+                    console.log('User does not own the NFT.');
+                    // Display a message or lock the content
+                }
+            });
+        } catch (error) {
+            console.error('User denied account access');
+        }
+    } else {
+        console.log('Non-Ethereum browser detected. You should consider trying MetaMask!');
+    }
+});
 
 
